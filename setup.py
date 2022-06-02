84294ba830bbad3a318d084f81cc1408e1658d67
Alexander Loginov <alex.loginov@mywish.io>
2022-06-02 16:19:18 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 META_DATA = dict(
     name="near-api-py",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
 
-    author="NEAR Inc",
+    author="NEAR Inc, MyWish.io",
 
-    url="https://github.com/near/near-api-py",
+    url="https://github.com/MyWishPlatform/near-api-py",
 
     packages=find_packages(),
 
